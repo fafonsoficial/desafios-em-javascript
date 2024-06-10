9d41e87e64717eeac1d0fdb5384f63823cced9a3
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-06-10 16:03:55 +0100

    Terceiro treimento de javaScript

---
+++ des025/desafios025.js
@@ -0,0 +1,7 @@
+var carro = 'Mercedes'
+console.log(`o seu carro é da marca ${carro}`)
+if (carro != 'Toyota') {
+    console.log('O seu carro não é muito bom')
+} else {
+    console.log('O seu carro é bom')
+}
---
+++ des025/desafios025a.js
@@ -0,0 +1,7 @@
+var nome = 'Edlise'
+console.log(`Seja muito bem-vinda ${nome}`)
+if (nome != 'Francisco'){
+    console.log('Não tens um nome verdadeiro')
+} else {
+    console.log('Tens o nome verdadeiro')
+}
