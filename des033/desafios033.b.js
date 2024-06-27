bde777c39c5c91d6deef29712faa3cda6d163798
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-06-27 18:54:50 +0100

    Começando a criar codigos em javascrip em forma de site

---
+++ des033/desafios033.b.js
@@ -0,0 +1,7 @@
+let idade = 12
+console.log(`Você está com ${idade} anos de idade`) //primeira impressão na tela
+if(idade >= 18 ) {
+    console.log('E você já é um jovem')
+} else {
+    console.log('E você ainda é um adoleicente')
+}
---
+++ des033/desafios033.c.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Você está morando em ${país}`)
+if(país == 'Angola') {
+    console.log('Você é Angolano')
+} else {
+    console.log('Você é estrangeiro')
+}
