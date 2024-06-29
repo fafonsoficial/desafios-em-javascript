4fc0511afd8188fbc99617466af4167e61837b8a
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-06-29 15:43:59 +0100

    Praticando novamente

---
+++ des036/desafios036.js
@@ -0,0 +1,10 @@
+var idade = 15
+if (idade < 16) {
+    console.log('Não pode vota')
+} else {
+    if(idade >= 16 && idade < 18) {
+        console.log('voto opcionar')
+    } else [
+        console.log('Voto Nulo')
+    ]
+}
