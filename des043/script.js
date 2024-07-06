9d600845935c5ca9ea52943516232ebd89ea8ac7
Francisco Afonso <fafonsoficial@gmail.com>
2024-07-06 23:30:20 +0100

    Voltando a treinar

--- des043/script.js
+++ des043/script.js
@@ -4,9 +4,47 @@ function verificar() {
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
 
-    if(fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || fano.value > ano) {
         alert('[ERRO] Verifica os dados e tente novamente!')
     } else {
-        confirm('Dados Introduzidos Validos')
+        let fsex = document.getElementsByName('radsex')
+        let idade = ano - Number(fano.value)
+        let gênero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // CRIANÇA
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade >= 10 && idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                // IDOSO
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //CRIANÇA
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade >= 10 && idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
+
 }
