eb822d81033dcfc2820f8af5bba6f29dfa3a03ae
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-06 12:25:59 +0100

    Terminado

--- des042/script.js
+++ des042/script.js
@@ -8,10 +8,12 @@ function Carregar() {
     msg.innerHTML = `Neste exato momento são ${hora}:${min}:${seg}`
     if(hora >= 0 && hora < 12) {
         img.src = 'foto-manha.png'
+        document.body.style.background = '#6f380a'
     } else if(hora >= 12 && hora < 18) {
-
         img.src = 'foto-meiodia.png'
+        document.body.style.background = '#ee9a21'
     } else {
         img.src = 'foto-noite.png'
+        document.body.style.background = '#283e49'
     }
 }
