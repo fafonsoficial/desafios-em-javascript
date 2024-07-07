f68a79a345c63d5f456582390cab449a035d15d0
Francisco Afonso <fafonsoficial@gmail.com>
2024-07-07 01:55:36 +0100

    Organizando o projeto

--- des051/script.js
+++ des051/script.js
@@ -1,3 +1,14 @@
-function verificar(){
+function verificar() {
+    var nome = document.getElementById('nome')
+    var média = docuemnt.getElementById('nota')
+    var nome1 = (nome.value)
+    var média1 = (média.value)
+    
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('input#nasc')
+    var res = document.querySelector('div#res')
+
+
     
 }
