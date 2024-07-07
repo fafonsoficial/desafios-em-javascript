5068303a8e914af89d667c8902485dec5d7b057a
Francisco Afonso <fafonsoficial@gmail.com>
2024-07-07 22:02:56 +0100

    Organizando o script

--- des051/script.js
+++ des051/script.js
@@ -1,16 +1,23 @@
 function verificar() {
     var pessoa = document.getElementById('nome')
-    var ano = document.getElementById('nasci')
     var res = document.getElementById('res')
     var média = document.getElementById('média')
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('nasci')
+
     var médiatotal = (média.value)
     var nome = (pessoa.value)
 
-    if (médiatotal == 0) {
+    if (fano.value == 0 || fano.value > ano ) {
         alert('[ERRO], Verifique o valor da média que inseriu!')
+    } else {
+        var sexo = document.getElementsByName('radsex')
     } else if (médiatotal > 7.5) {
         res.innerHTML = `${nome}, a sua candidatudo será processada!`
     } else {
         res.innerHTML = `${nome},a sua candidatura não poderá ser processada por ter uma média inferior a 7.5`
     }
 }
+médiatotal == 0
