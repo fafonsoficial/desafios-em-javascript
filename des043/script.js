20080444b10d2b87e1d3c2248bd54b173c204533
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-08 10:11:51 +0100

    Terminado

--- des043/script.js
+++ des043/script.js
@@ -4,9 +4,46 @@ function verificar() {
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
 
-    if(fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         alert('[ERRO] Verifica os dados e tente novamente!')
+        confirm('Introduza os dados novamente!')
     } else {
-        confirm('Dados Introduzidos Validos')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        if (fsex[0].Checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // CRIANÇA
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade >= 10 && idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                // IDOSO
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //CRIANÇA
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade >= 10 && idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
