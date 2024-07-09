60c0766e75d293cb8ecfb5394357b60283e108ef
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-09 23:26:54 +0100

    Melhorando cada ponto dos exercicios

--- des051/script.js
+++ des051/script.js
@@ -1,4 +1,14 @@
 function verificar() {
-    var identidade = document.getElementById('nome')
-    var data =  document.getElementById('datanascimento')
+
+    var identidade_a = document.getElementById('primnome')
+    var identidade_b = document.getElementById('sobnome')
+
+    var nomeValue1 = identidade_a.value
+    var nomeValue2 = identidade_b.value;
+
+    var data = document.getElementById('datanascimento')
+    
+    var res = document.getElementById('res')
+
+    
 }
