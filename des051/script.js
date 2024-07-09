ebe30be8b6fe470b1ab3b3b4c827b978e979bb71
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-09 23:30:20 +0100

    pontos a ponto da organização do codigo

--- des051/script.js
+++ des051/script.js
@@ -1,13 +1,13 @@
 function verificar() {
 
     var identidade_a = document.getElementById('primnome')
-    var identidade_b = document.getElementById('sobnome')
-
+    var identidade_b = document.getElementById('sobrenome')
     var nomeValue1 = identidade_a.value
-    var nomeValue2 = identidade_b.value;
+    var nomeValue2 = identidade_b.value
 
     var data = document.getElementById('datanascimento')
-    
+    var dataValue = data.value
+
     var res = document.getElementById('res')
 
     
