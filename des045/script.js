1f35194647fb1adee12a80ce054528b1e05ed7be
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-16 00:36:04 +0100

    Organizando

--- des045/script.js
+++ des045/script.js
@@ -1,1 +1,20 @@
-alert('Ola')
+function gerar() {
+    var num = document.getElementById('txtnum')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        alert('Por favor digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 12) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
