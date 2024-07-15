d5e67c50884f93dec98f235dbecc9d6fd3d8b0b5
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-15 22:23:50 +0100

    Organizando

--- des049/node_31c5b9d00a5ee.tmp.js
+++
@@ -1,4 +0,0 @@
-var comerpizza = 8
-while (comerpizza < 10) {
-    console.log('Pode comer a vontade')
-}
--- des049/node_b9218004eef65.tmp.js
+++
@@ -1,4 +0,0 @@
-var comerpizza = 1
-while (comerpizza < 10) {
-    console.log('Pode comer a vontade')
-}
