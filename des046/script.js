a423b24ed39a7e33cd82b55c795588596dde8f6e
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-18 19:28:55 +0100

    Organizando Tudo novamente

--- des046/script.js
+++ des046/script.js
@@ -1,14 +1,9 @@
 function convidar() {
     let nome = document.getElementById('txtnam')
     let res = document.getElementById('res')
-    let presenca = document.getElementById('txtpre')
-
+   
     let convidado = String(nome.value)
     let confirm = String(presenca.value)
 
-    if (confirm == 0){
-        alert(`[ATENÇÃO] ${convidado}, por favor confirme a sua presença usando S ou N`)
-    }
-
-    res.innerHTML = `O nome convidado é o ${convidado} e ela consirmou com ${confirm}`
+    
 }
