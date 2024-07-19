54ccda6e49ee980650279e5baa378900ddc2d564
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-19 03:09:55 +0100

    Exercico be feito

--- des046/script.js
+++ des046/script.js
@@ -1,9 +1,26 @@
-function convidar() {
-    let nome = document.getElementById('txtnam')
-    let res = document.getElementById('res')
-   
-    let convidado = String(nome.value)
-    let confirm = String(presenca.value)
+function consultar() {
+    var numConvidado = document.getElementById('txtnum')
+    var res = document.getElementById('res')
 
-    
+    var numValue = Number(numConvidado.value);
+
+    if (numValue == 0) {
+        alert('[ATENÇÃO] Indique o número de convidados no seu convite físico!')
+        confirm('Daremos continuidade a nossa consulta.')
+    } else if (numValue <= 10) {
+        document.body.style.background = 'green'
+        alert('Indica o nome de cada um deles')
+        var nome = prompt('Indique o nome: ')
+        res.innerHTML = `Número de convidados aceites! E o convidado principal chame-se ${nome}`
+    } else {
+        res.innerHTML = 'Número de convidados recusados!'
+        document.body.style.background = 'red'
+    }
+
 }
+
+/*
+for (var numValue = 1; numValue <= 5; numValue++) {
+            res.innerHTML =+ `O ${numValue} é o ${nome}`
+        }
+*/
