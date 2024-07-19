3ccb0b033bf1d9ce104c33c934110fc89c685bc5
Francisco Ngonde Afonso <fafonsoficial@gmail.com>
2024-07-19 03:22:18 +0100

    Concluido

--- des046/script.js
+++ des046/script.js
@@ -7,11 +7,11 @@ function consultar() {
     if (numValue == 0) {
         alert('[ATENÇÃO] Indique o número de convidados no seu convite físico!')
         confirm('Daremos continuidade a nossa consulta.')
-    } else if (numValue <= 10) {
-        document.body.style.background = 'green'
+    } else if (numValue <= 5) {
         alert('Indica o nome de cada um deles')
         var nome = prompt('Indique o nome: ')
         res.innerHTML = `Número de convidados aceites! E o convidado principal chame-se ${nome}`
+        document.body.style.background = 'green'
     } else {
         res.innerHTML = 'Número de convidados recusados!'
         document.body.style.background = 'red'
